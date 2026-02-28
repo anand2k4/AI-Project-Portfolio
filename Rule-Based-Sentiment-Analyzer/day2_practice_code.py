77c29552db3b3557572ae75dfa19bcf83af7c2a5
anand2k4 <99279033+anand2k4@users.noreply.github.com>
2026-02-28 22:36:51 +0530

    Add files via upload

---
+++ Rule-Based-Sentiment-Analyzer/day2_practice_code.py
@@ -0,0 +1,21 @@
+           #  List
+# Example
+reviews= ["Good movie," "Bad acting","Amazing story"]
+print(reviews)
+
+# Access items:
+print(reviews[0])  
+print(reviews[1])
+
+# Add item: 
+reviews.append("Excellent direction")
+
+# Lenght:
+print(len(reviews))
+
+# Loop : for loop: (going through each item one by one)
+# while loops repeatedly excecute a block of code as long as a given condition remains true.
+for review in reviews:
+    print(review)
+
+
